7b52ca06f939ce92f0b178c9344a733d5e476cf1
unknown <muhtorxonovamashxura@gmail.com>
2026-02-02 23:50:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Mashhura from './components/mashhura/Mashhura'
+
+const App = () => {
+  return (
+    <div>
+      <Mashhura/>
+    </div>
+  )
+}
+
+export default App

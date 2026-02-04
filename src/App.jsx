81318354edfb43d75df8e2aea33138c665ea8606
unknown <muhtorxonovamashxura@gmail.com>
2026-02-04 23:37:14 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Mashhura from './components/mashhura/Mashhura'
+import Sidebar from './components/mashhura/Sidebar'
 
 const App = () => {
   return (
     <div>
       <Mashhura/>
+      <Sidebar/>
     </div>
   )
 }
